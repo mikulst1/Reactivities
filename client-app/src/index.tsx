3aa7ad8723ca76b3b4b82c1f4bd807e7cf8d6ac2
Stefan Mikulics <SMikuli@ra.rockwell.com>
2020-11-05 11:42:05 +0100

    add error handling

---
+++ client-app/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app/layout/styles.css';
+import App from './app/layout/App';
+import * as serviceWorker from './serviceWorker';
+import { Router } from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import 'react-toastify/dist/ReactToastify.min.css'
+import ScrollToTop from './app/layout/ScrollToTop';
+
+export const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history = {history}>
+    <ScrollToTop>
+        <App />
+    </ScrollToTop>
+    </Router>
+, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
